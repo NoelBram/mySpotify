fb246158cf02f13c991ff48dea996165cb83c583
Noel Brambila <noelbrambila@yahoo.com>
2022-09-29 23:24:10 -0700

    added new get command for collecting user playlists

---
+++ database/getAPI.py
@@ -0,0 +1,146 @@
+import json
+import requests
+import base64
+from secrets import *
+
+from requests.api import head
+
+authUrl = "https://accounts.spotify.com/api/token"
+authHeader = {}
+authData = {}
+
+def getAccessToken(clientID, clientSecret):
+    message = f"{clientID}:{clientSecret}" # secret.py에 사용자 정보 불러오기
+    message_bytes= message.encode('ascii') # 메세지 ascii code로 인코딩
+    base64_bytes = base64.b64encode(message_bytes)  
+    base64_message = base64_bytes.decode('ascii')
+
+    # print(base64_message) 
+    # curl -X "POST" -H "Autorization: Basic Zj....Y0NDQ=" -d grant_type=client_credentials https://accounts.spotify.com/api/token
+    # 반드시 Basic 뒤에 한칸 뛰어야 합니다.
+
+    authHeader['Authorization'] = "Basic " + base64_message
+    authData['grant_type'] = "client_credentials"
+
+    res = requests.post(authUrl, headers=authHeader, data=authData)
+
+    print(res) # <Response [200]> 
+
+    responseObject = res.json() # json 파일화 시키기
+    # print(json.dumps(responseObject, indent=2))  # indent 옵션 : JSON 문자열을 읽기 편하게 할 필요가 있을 경우
+
+    # 토큰을 받았으면, 아래와 같이 토큰을 가져올 수 있습니다.
+
+    accessToken = responseObject['access_token']
+
+    return accessToken
+
+token = getAccessToken(clientID, clientSecret)
+
+# print(token)
+# access_token = ??
+
+"""
+## Find Tracks Audio Functions
+Link  : https://developer.spotify.com/documentation/web-api/reference/#/operations/get-audio-features
+
+"""
+
+# def getTrackFunctions(token, id):
+#     endpoint = f"http://api.spotify.com/v1/audio-features/{id}"
+
+#     getHeader = {
+#         "Authorization" : "Bearer " + token
+#     }
+#     res = requests.get(endpoint, headers=getHeader)
+
+#     trackinfo = res.json()
+
+#     return trackinfo
+
+# id = "2g0LdZQce9xlcHb1mBJyuz"
+# trackInfo = getTrackFunctions(token, id)
+
+# with open('trackinfo.json','w') as f: #json 파일로 저장
+#     json.dump(trackInfo, f)
+
+
+# -------------------------------------
+
+user_id = "annieboyse"
+
+def getUserPlaylist(token, user_id): 
+    endpoint = f"https://api.spotify.com/v1/users/{user_id}/playlists"
+    getHeader = {
+      "Authorization" : "Bearer " + token
+       }
+    
+    res_users = requests.get(endpoint, headers=getHeader)
+    users_plists = res_users.json()
+
+    plist_titles = []
+    for plist_title in range(0, len(users_plists['items'])) : 
+        plist_titles.append(users_plists['items'][plist_title]["name"])
+
+    plist_dict = {}
+    for i in range(0, len(plist_titles)) :
+        plist_dict[f'{plist_titles[i]}'] = users_plists['items'][i]["id"]
+
+    del plist_dict['saturday morning baking <33']
+    del plist_dict['riot grrrl ']
+    del plist_dict['happy brain chemicals go brrr ']
+    del plist_dict['armin\'s playlist']
+
+    return plist_dict, users_plists
+
+plist_dicts, users_plists  = getUserPlaylist(token, user_id)
+
+with open(f'./json_data/MBTI_plist1.json','w') as f:
+    json.dump(plist_dicts, f)
+
+
+def getPlaylistTracks(token, playlist_id):
+
+    endpoint = f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks"
+
+    getHeader = {
+        "Authorization" : "Bearer " + token
+    }
+
+    res = requests.get(endpoint, headers=getHeader)
+    playlist_track = res.json()
+
+    return playlist_track
+
+
+pli_track = {}
+track_dic = {}
+
+for i in range(0, len(list(plist_dicts.values()))):
+    tracks = getPlaylistTracks(token, list(plist_dicts.values())[i]) # 위 함수를 이용하여 `tracks`에 저장한다.
+    for j in range(0, len(tracks['items'])): # MBTI 별 트랙에서 `{순서 : track_id}` 순으로 json 피클링을 한다.
+        track_dic[f'{j}'] = tracks['items'][j]['track']['id']
+    # with open(f'./json_data/mbti_track/{list(plist_dicts.keys())[i].split()[0]}.json','w') as f:
+    #     json.dump(track_dic, f)
+    pli_track[f"{list(plist_dicts.keys())[i].split()[0]}"] = track_dic
+
+
+# with open(f'./json_data/mbti_track/mbti_tracks.json','w') as f:
+#    json.dump(pli_track, f)
+
+
+def gettrackAudioFeatures(token, ids):
+    """
+    ## 트랙에 대한 정보 가져오기
+
+    """
+    endpoint = "https://api.spotify.com/v1/audio-features?ids={ids}"
+    
+    getHeader = {
+        "Authorization" : "Bearer " + token
+    }
+
+    res = requests.get(endpoint, headers=getHeader)
+    track_features = res.json()
+    
+    return track_features
--- database/main.py
+++ database/main.py
@@ -1,5 +1,7 @@
 from contextlib import nullcontext
+import copy
 import sqlite3
+from xml.etree.ElementTree import tostring
 # from flask import Flask
 # import sqlalchemy
 import pandas as pd 
@@ -10,9 +12,9 @@ from datetime import datetime
 import datetime
 # from keyvalue_sqlite import KeyValueSqlite
 
-DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
-USER_ID = "Noah Bram" # your Spotify username 
-TOKEN = "BQANn_Uo8Ge5pHHXYsyWq4r1jJEXzE6sPzJ_bMzpQTKMWOx10OzPTKVuPQ7vFu4lkn40C3XPeUl7LZz9IHBkjeXNx0_ywlSqRNxKa8HZ6hFbKfn_lRxyB7AZzI_-nkQJeBBoAn_7bAvl1QF4G9Cth4fM2v18lp6_tk7OHxLoQM8gNxK21_WytPn8xll-e0FlZtFTJORT" # your Spotify API token
+DATABASE_LOCATION = "sqlite:///my_playlists.sqlite"
+USER_ID = "22io3oxgaphrgsxto4naqh4ai"
+TOKEN = "BQDrkxB3B3Vvx_As1e4pTEmcmSJPQQCwJqnB1yUVnc3xjOswZs1JNtI2Yfhautf6TbonP08_g2iicV35GGBMuqqVm5sSGSE0-1ZYRj5-yN5zryIyfFeymxxZOeh31Y7Y6SD82JFE5WJQy8xkejN8Do6rAGGpTTyRwJkQiqXifBWj27AE5JeZRSJydmKOcXZ2UNorXXq9rupT0R76sFM" # your Spotify API token
 
 # Generate your token here:  https://developer.spotify.com/console/get-recently-played/
 # Note: You need a Spotify account (can be easily created for free)
@@ -20,7 +22,7 @@ TOKEN = "BQANn_Uo8Ge5pHHXYsyWq4r1jJEXzE6sPzJ_bMzpQTKMWOx10OzPTKVuPQ7vFu4lkn40C3X
 def check_if_valid_data(df: pd.DataFrame) -> bool:
     # Check if dataframe is empty
     if df.empty:
-        print("No songs downloaded. Finishing execution")
+        print("No playlists downloaded. Finishing execution")
         return False 
 
     # Primary Key Check
@@ -40,7 +42,7 @@ def check_if_valid_data(df: pd.DataFrame) -> bool:
     # timestamps = df["timestamp"].tolist()
     # for timestamp in timestamps:
     #     if datetime.datetime.strptime(timestamp, '%Y-%m-%d') != yesterday:
-    #         raise Exception("At least one of the returned songs does not have a yesterday's timestamp")
+    #         raise Exception("At least one of the returned playlists does not have a yesterday's timestamp")
             
 
     return True
@@ -48,82 +50,104 @@ def check_if_valid_data(df: pd.DataFrame) -> bool:
 if __name__ == "__main__":
 
     # Extract part of the ETL process
- 
+    endpoint = "https://api.spotify.com/v1/users/{user_id}/playlists".format(user_id = USER_ID)
     headers = {
         "Accept" : "application/json",
         "Content-Type" : "application/json",
         "Authorization" : "Bearer {token}".format(token=TOKEN)
     }
     
-    # Convert time to Unix timestamp in miliseconds      
-    today = datetime.datetime.now()
-    yesterday = today - datetime.timedelta(days=60)
-    yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
+    # # Convert time to Unix timestamp in miliseconds      
+    # today = datetime.datetime.now()
+    # yesterday = today - datetime.timedelta(days=60)
+    # yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
 
-    # Download all songs you've listened to "after yesterday", which means in the last 24 hours      
-    r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix_timestamp), headers = headers)
+    # Download all playlists you've listened to "after yesterday", which means in the last 24 hours      
+    # r = requests.get(endpoint, headers = headers)
+    user_playlists_request = requests.get(endpoint, headers = headers)
 
-    data = r.json()
+    user_playlists = user_playlists_request.json()
 
-    song_names = []
-    artist_names = []
-    played_at_list = []
-    timestamps = []
+    mixtape_choices = ["Daily Mix 1", "Daily Mix 2", "Daily Mix 3", "Daily Mix 4", "Daily Mix 5", "Daily Mix 6", "Discover Weekly", "Release Radar"]
+    playlist_titles = []
+    for title in range(0, len(user_playlists['items'])) : 
+        if user_playlists['items'][title]["name"] in mixtape_choices:
+            playlist_titles.append(user_playlists['items'][title]["name"])
+    
+    playlist_dict = {}
+    for i in range(len(playlist_titles)) :
+        playlist_dict[i] = {user_playlists['items'][i]["id"] : {f'{playlist_titles[i]}', {}}}
 
-    # Extracting only the relevant bits of data from the json object      
-    for song in data["items"]:
-        song_names.append(song["track"]["name"])
-        artist_names.append(song["track"]["album"]["artists"][0]["name"])
-        played_at_list.append(song["played_at"])
-        timestamps.append(song["played_at"][0:10])
+    print(playlist_dict)
+
+    # playlist_url = []
+    
+
+    # # Extracting only the relevant bits of data from the json object      
+    # for playlist in data:
+    #     playlist_id.append(playlist["items"]["id"])
+    #     playlist_names.append(playlist["items"][0]["name"])
+    #     playlist_url.append(playlist["items"][0]["external_urls"]["spotify"])
+    #     tracks[playlist].append(playlist["items"][0]["tracks"]["name"])
         
-    # Prepare a dictionary in order to turn it into a pandas dataframe below       
-    song_dict = {
-        "song_name" : song_names,
-        "artist_name": artist_names,
-        "played_at" : played_at_list,
-        "timestamp" : timestamps
+        
+    # # Prepare a dictionary in order to turn it into a pandas dataframe below       
+    # playlist_dict = {
+    #     "playlist_id" : playlist_id,
+    #     "playlist_name" : playlist_names,
+    #     "playlist_url" : playlist_url,
+    #     "tracks": tracks
+    # }
+
+    # playlist_df = pd.DataFrame(playlist_dict, columns = ["playlist_id", "playlist_name", "playlist_url", "tracks"])
+    
+    # print(playlist_df)
+
+    # # Validate
+    # if check_if_valid_data(playlist_df):
+    #     print("Data valid, proceed to Load stage")
+
+    # # engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    # conn = sqlite3.connect('my_playlists.sqlite')
+    # # cursor = conn.cursor()
+
+    # # sql_query = """
+    # # CREATE TABLE IF NOT EXISTS my_playlists(
+    # #     playlist_name VARCHAR(200),
+    # #     artist_name VARCHAR(200),
+    # #     played_at VARCHAR(200),
+    # #     timestamp VARCHAR(200),
+    # #     CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+    # # )
+    # # """
+
+    # # sql_query_two = """insert or replace into my_playlists (playlist_name,artist_name,played_at,timestamp) values (?,?,?)"""
+
+    # # cursor.execute(sql_query)
+    # # cursor.execute(sql_query_two)
+
+    # print("Opened database successfully")
+
+    # try:
+    #     playlist_df.to_sql("my_playlists", conn, index=False, if_exists='replace')
+    # except:
+    #     print("Data already exists in the database")
+
+    # # df = pd.read_sql_query('SELECT * FROM my_playlists', conn, parse_dates=["playlist_name"])
+
+    # conn.close()
+    # print("Close database successfully")
+    # # print(df.head)
+    
+    # # Job scheduling 
+def gettrackAudioFeatures(token, ids):
+    endpoint = "https://api.spotify.com/v1/audio-features?ids={ids}"
+    
+    getHeader = {
+        "Authorization" : "Bearer " + token
     }
 
-    song_df = pd.DataFrame(song_dict, columns = ["song_name", "artist_name", "played_at", "timestamp"])
+    res = requests.get(endpoint, headers=getHeader)
+    track_features = res.json()
     
-    print(song_df)
-
-    # Validate
-    if check_if_valid_data(song_df):
-        print("Data valid, proceed to Load stage")
-
-    # engine = sqlalchemy.create_engine(DATABASE_LOCATION)
-    conn = sqlite3.connect('my_played_tracks.sqlite')
-    # cursor = conn.cursor()
-
-    # sql_query = """
-    # CREATE TABLE IF NOT EXISTS my_played_tracks(
-    #     song_name VARCHAR(200),
-    #     artist_name VARCHAR(200),
-    #     played_at VARCHAR(200),
-    #     timestamp VARCHAR(200),
-    #     CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
-    # )
-    # """
-
-    # sql_query_two = """insert or replace into my_played_tracks (song_name,artist_name,played_at,timestamp) values (?,?,?)"""
-
-    # cursor.execute(sql_query)
-    # cursor.execute(sql_query_two)
-
-    print("Opened database successfully")
-
-    try:
-        song_df.to_sql("my_played_tracks", conn, index=False, if_exists='replace')
-    except:
-        print("Data already exists in the database")
-
-    # df = pd.read_sql_query('SELECT * FROM my_played_tracks', conn, parse_dates=["song_name"])
-
-    conn.close()
-    print("Close database successfully")
-    # print(df.head)
-    
-    # Job scheduling 
-    
+    return track_features
